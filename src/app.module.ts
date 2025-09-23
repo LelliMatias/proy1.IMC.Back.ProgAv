30e90fd872bdbc7049505086803977e950e48088
Matias Lelli <115432477+LelliMatias@users.noreply.github.com>
2025-09-22 21:28:42 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,26 +8,26 @@ import { ImcResult } from './module/imc/imc.entity'; // tu entidad
 @Module({
   imports: [
     // PRODUCCION
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   url: process.env.DATABASE_URL,
-    //   autoLoadEntities: true,
-    //   synchronize: false,
-    //   retryAttempts: 10,
-    //   retryDelay: 3000,
-    // }),
-    // DESARROLLO
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'imcdb',
-      entities: [ImcResult],
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: false,
+      retryAttempts: 10,
+      retryDelay: 3000,
     }),
+    // DESARROLLO
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'imcdb',
+    //   entities: [ImcResult],
+    // }),
 
-    ImcModule // tu módulo feature
+    ImcModule 
   ],
   controllers: [AppController],
   providers: [AppService],
